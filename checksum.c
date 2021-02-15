29274726a67ce1a469ec660d1a120796f155301a
Grantas J <grantasextreme@gmail.com>
2021-02-14 22:54:37 -0800

    message 2

---
+++ checksum.c
@@ -0,0 +1,26 @@
+/********************************/
+/* Program Name:                */
+/* Author:                      */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+#define max_int (255)
+#define byte (char)
+
+int main (int argc, char * argv[], char ** envp) {
+
+  int count = 10;
+  int sum = 0;   
+  byte checksum; 
+  byte complement;
+
+  /* the following is the prototype for the read system call */
+  /* int read(int fildes, void *buf, size_t nbyte);  */
